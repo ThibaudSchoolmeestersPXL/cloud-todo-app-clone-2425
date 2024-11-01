c5558aa85b5e44fe73874222d71a56aac566c868
ThibaudSchoolmeestersPXL <116737392+ThibaudSchoolmeestersPXL@users.noreply.github.com>
2024-11-01 17:06:11 +0100

    carrousel updated

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const axios = require('axios');
+const parser = require('xml2json');
 const carrouselRouter = express.Router();
-const carrouselData = require('./../data/carrousel.json');
 
-carrouselRouter.get('', (req, res) => {
-    res.json(carrouselData);
+carrouselRouter.get('', async (req, res) => {
+  try {
+    const url = `https://stage-bucket-thibaud.s3.amazonaws.com/`;
+
+    const response = await axios.get(url);
+    const xmlData = response.data;
+
+    const jsonData = JSON.parse(parser.toJson(xmlData));
+    const contents = jsonData.ListBucketResult.Contents;
+
+    const imageUrls = contents.map(content => {
+      return {
+        url: `${url}${content.Key}`
+      };
+    });
+
+    res.json(imageUrls);
+  } catch (error) {
+    console.error('Error retrieving images from AWS S3:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
-
 
 module.exports = carrouselRouter;
